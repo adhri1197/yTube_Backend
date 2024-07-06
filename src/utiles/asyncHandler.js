8c89a5aa943ce1f169cdd2690ab14e3f9ffcea73
adhri1197 <anirudhvarhan007@gmail.com>
2024-07-06 23:52:03 +0530

    utils code

---
+++ src/utiles/asyncHandler.js
@@ -0,0 +1,21 @@
+// const asyncHandler = (fn) =>(req, res, next) =>{
+//     try {
+        
+//     } catch (error) {
+//         res.status(err.code || 500).json
+//         success: false
+//         message: err.messaged
+        
+//     }  // this is a try catch function their is a another funtion 
+//     // which known as Promise funtion 
+// }
+
+// Promish Funtion 
+const asyncHandler = (requestHandler) =>{ 
+    (req, res, next) =>{
+        Promise.resolve(requestHandler(req, res, next)).catch
+        ((err) => next(err))
+    }
+}
+
+    export {asyncHandler}
