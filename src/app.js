d063987b6d549554b0042d86e260719c3f499576
adhri1197 <anirudhvarhan007@gmail.com>
2024-07-14 01:53:41 +0530

    creating multer middleware

--- src/app.js
+++ src/app.js
@@ -4,14 +4,13 @@ import cookieParser from "cookie-parser"
 
 const app = express()
 
-app.cors(cors({
+app.use(cors({
     origin: process.env.CORS_ORIGIN,
-    credentials:true
+    credentials: true
 }))
 
-app.use(express.json({limit:"20kb"}))
-app.use(express.urlencoded({extended: true,
-limit: "16kb"}))
+app.use(express.json({limit: "16kb"}))
+app.use(express.urlencoded({extended: true, limit: "16kb"}))
 app.use(express.static("public"))
-app.use(cookieParser)
+app.use(cookieParser())
 export { app }
