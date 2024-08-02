dc8095ed0e3209cafcc32172da5c3a53c856ac14
adhri1197 <anirudhvarhan007@gmail.com>
2024-08-03 00:11:53 +0530

    update user-controller subscription

---
+++ src/models/subscription_model.js
@@ -0,0 +1,11 @@
+import mongoose, {Schema} from "mongoose";
+
+
+const SubscriptionSchema = new Schema({
+    s
+
+})
+
+
+export const Subscription = mongoose.model("Subscription",
+    SubscriptionSchema)
